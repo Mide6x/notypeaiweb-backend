26e98721e7aa7e385f3496ea459b31cef079748c
mide6x <adewoleolumide05@gmail.com>
2024-12-25 12:03:04 +0100

    Added multiple urls for prod

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://notypeai.com', 'https://www.notypeai.com']
+    ? ['https://notypeai.com', 'https://www.notypeai.com', 'https://notypeaiweb-backend.onrender.com']
     : 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
